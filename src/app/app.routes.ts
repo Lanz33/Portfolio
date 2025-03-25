d945659f85f70ba6f17fbcc4f13abc3bb870d0ab
Christian Fischer <lanz33@gmail.com>
2025-03-25 12:06:17 +0100

    work on portrait

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { LandingsiteComponent } from './landingsite/landingsite.component';
 
 export const routes: Routes = [
-    { path:'', component: LandingsiteComponent} 
+    { path:'', component: LandingsiteComponent}, 
+    { path: '**', redirectTo: 'login' }
 ];
