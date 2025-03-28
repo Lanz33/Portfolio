77679437d35fca3b7efa67edf7646be67e57d35d
Christian Fischer <lanz33@gmail.com>
2025-03-28 13:09:05 +0100

    add skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
+interface Skill {
+  title: string;
+  pfad: string;
+}
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
+  skills: Skill[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<Skill[]>('assets/skills.json').subscribe(data => {
+      this.skills = data;
+    });
+  }
 }
