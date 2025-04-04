338ba4262f0f621252845475bc655e711e813e4b
Christian Fischer <lanz33@gmail.com>
2025-04-04 15:05:31 +0200

    add animation portfolio and contact-section

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, model } from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatCheckboxModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactComponent {
-
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  
+  readonly checked = model(false);
+  readonly indeterminate = model(false);
+  readonly labelPosition = model<'before' | 'after'>('after');
+  readonly disabled = model(false);
 }
